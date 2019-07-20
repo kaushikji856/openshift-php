841fb1da833ed72f2605c5809ba9556b5aabc8b0
kaushikji856 <42434586+kaushikji856@users.noreply.github.com>
2019-07-20 14:09:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 #  phpinfo();
 echo "Welcome to Vipin's OpenShift online portal with Nikhil ";
+echo "Welcome to Vipin's OpenShift online portal with Nikhil ";
+echo "Welcome to Vipin's OpenShift online portal with Nikhil ";echo "Welcome to Vipin's OpenShift online portal with Nikhil ";
+echo "Welcome to Vipin's OpenShift online portal with Nikhil ";
 ?>
